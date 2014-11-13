13d5884d583c41a1257a30b9a30a091b2b918715
wianvos <wvos@xebia.com>
2014-11-13 22:21:47 +0100

    added some ci stuff

--- lib/puppet/provider/xlrelease_xld_server/rest.rb
+++ lib/puppet/provider/xlrelease_xld_server/rest.rb
@@ -5,7 +5,7 @@ Puppet::Type.type(:xlrelease_xld_server).provide :rest, :parent => Puppet::Provi
   has_feature :restclient
 
   def create
-
+    p "create"
     ci_json = to_json(resource[:id],resource[:type],resource[:properties])
     p rest_post "/xldeploy/servers", ci_json
     rest_post "/xldeploy/servers", ci_json
@@ -13,14 +13,17 @@ Puppet::Type.type(:xlrelease_xld_server).provide :rest, :parent => Puppet::Provi
   end
 
   def destroy
+    p "desroy"
     rest_delete "deployit/servers/#{resource[:id]}"
   end
 
   def exists?
+    p "exists"
     resource_exists?(resource[:id])
   end
 
   def properties
+    p "properties"
     ci_json = rest_get "deployit/servers/#{resource[:id]}"
     ci_hash = to_hash(ci_json)
 
@@ -37,11 +40,13 @@ Puppet::Type.type(:xlrelease_xld_server).provide :rest, :parent => Puppet::Provi
   end
 
   def properties=(value)
+    p "properties="
     ci_json = to_json(resource[:id],resource[:type],resource[:properties])
     rest_put "deployit/servers/#{resource[:id]}", ci_json
   end
 
   def resource_exists?(id)
+    p "res_exsists"
     # check if a certain resource exists in the XL Deploy repository
     ci_exists?(id)
   end
