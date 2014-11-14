9197570cbeac46f0c95db82faf91696a8985c2b9
wianvos <wvos@xebia.com>
2014-11-14 21:27:22 +0100

    added some ci stuff

--- lib/puppet/provider/xlr_rest_provider.rb
+++ lib/puppet/provider/xlr_rest_provider.rb
@@ -2,28 +2,8 @@ require 'puppet'
 require 'open-uri'
 require 'net/http'
 require 'json'
-require 'pp'
 class Puppet::Provider::XLReleaseRestProvider < Puppet::Provider
 
-  # def rest_get(service)
-  #   begin
-  #     response = RestClient.get "#{resource[:rest_url]}/#{service}", {:accept => :xml, :content_type => :xml }
-  #   rescue Exception => e
-  #     return e.message
-  #   end
-  # end
-  #
-  # def rest_post(service, body='')
-  #   RestClient.post "#{resource[:rest_url]}/#{service}", body, {:content_type => :xml }
-  # end
-  #
-  # def rest_put(service, body='')
-  #   RestClient.put "#{resource[:rest_url]}/#{service}", body, {:content_type => :xml }
-  # end
-  #
-  # def rest_delete(service)
-  #   RestClient.delete "#{resource[:rest_url]}/#{service}", {:accept => :xml, :content_type => :xml }
-  # end
 
   def rest_get(service)
     execute_rest(service, 'get')
@@ -58,16 +38,14 @@ class Puppet::Provider::XLReleaseRestProvider < Puppet::Provider
   def execute_rest(service, method, body='')
 
     uri = URI.parse("#{resource[:rest_url]}/#{service}")
-    p uri
     http = Net::HTTP.new(uri.host, uri.port)
-    p http
     request = case method
                 when 'get'    then Net::HTTP::Get.new(uri.request_uri)
                 when 'post'   then Net::HTTP::Post.new(uri.request_uri)
                 when 'put'    then Net::HTTP::Put.new(uri.request_uri)
                 when 'delete' then Net::HTTP::Delete.new(uri.request_uri)
               end
-    p request
+
     #p request.pretty_print_inspect
 
     request.basic_auth(uri.user, uri.password) if uri.user and uri.password
@@ -77,7 +55,7 @@ class Puppet::Provider::XLReleaseRestProvider < Puppet::Provider
     begin
 
       res = http.request(request)
-      p res
+
       raise Puppet::Error, "cannot send request to deployit server #{res.code}/#{res.message}:#{res.body}" unless res.is_a?(Net::HTTPSuccess)
       return res.body
     rescue Exception => e
--- lib/puppet/provider/xlrelease_xld_server/rest.rb
+++ lib/puppet/provider/xlrelease_xld_server/rest.rb
@@ -5,24 +5,20 @@ Puppet::Type.type(:xlrelease_xld_server).provide :rest, :parent => Puppet::Provi
   has_feature :restclient
 
   def create
-    p "create"
     ci_json = to_j(resource[:id],resource[:type],resource[:properties])
     rest_post "deployit/servers", ci_json
 
   end
 
   def destroy
-    p "desroy"
     rest_delete "deployit/servers/#{resource[:id]}"
   end
 
   def exists?
-    p "exists"
     resource_exists?(resource[:id])
   end
 
   def properties
-    p "properties"
     ci_json = rest_get "repository/tree/#{resource[:id]}"
     ci_hash = to_hash(ci_json)
 
@@ -39,14 +35,12 @@ Puppet::Type.type(:xlrelease_xld_server).provide :rest, :parent => Puppet::Provi
   end
 
   def properties=(value)
-    p "properties="
     ci_json = to_j(resource[:id],resource[:type],resource[:properties])
     rest_put "deployit/servers/#{resource[:id]}", ci_json
   end
 
   def resource_exists?(id)
-    p "res_exsists"
-    # check if a certain resource exists in the XL Deploy repository
+    # check if a certain resource exists in the xl release repository
     ci_exists?(id)
   end
 end
