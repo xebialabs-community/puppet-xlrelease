7eb045ee31390648f294c5a4c4bb7afa15c3e9c7
wianvos <wvos@xebia.com>
2015-01-11 14:41:07 -0500

    typo correction

--- manifests/service.pp
+++ manifests/service.pp
@@ -22,26 +22,4 @@ class xlrelease::service {
     port => $xlr_port_number
   }
 
-#  xlrelease_xld_server{'xldeploy2':
-#    ensure      => 'present',
-#    type        => 'xlrelease.DeployitServerDefinition',
-#    rest_url    => 'http://admin:admin01@localhost:5516',
-#    properties  => {  "url"      => "http://10.20.1.4:4516/deployit2",
-#                      "username" => "admin",
-#                      "password" => "admin01" }
-#  } ->
-#  xlrelease_xld_server{'xldeploy':
-#    ensure      => 'present',
-#    type        => 'xlrelease.DeployitServerDefinition',
-#    rest_url    => 'http://admin:admin01@localhost:5516',
-#    properties  => {  "url"      => "http://10.20.1.4:4516/deployit",
-#                      "username" => "admin",
-#                      "password" => "admin01" }
-#  }
-  xlrelease_config_item{'jenkinst1':
-    ensure => 'present',
-    type   => 'jenkins.Server',
-    rest_url => 'http://admin:admin01@localhost:5516',
-
-  }
 }
